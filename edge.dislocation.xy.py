56da04e7a1fd276afc9c281c09bd06cecc677eb1
sakiv93 <vikas.sakiv93@gmail.com>
2019-04-24 01:18:16 +0200

    with all function and no multiple block plots

--- edge.dislocation.xy.py
+++ edge.dislocation.xy.py
@@ -4,6 +4,17 @@ def edge_stress_xy(x,y,shear_modulus,burgers_vector,poissons_ratio):
     D=shear_modulus*burgers_vector/(2*np.pi*(1-poissons_ratio))
     stress_xy=D*x*(x**2-y**2)/(x**2+y**2)**2
     return stress_xy
+def edge_stress_xx(x,y,shear_modulus,burgers_vector,poissons_ratio):
+    D=shear_modulus*burgers_vector/(2*np.pi*(1-poissons_ratio))
+    stress_xx=-D*y*(3*x**2+y**2)/(x**2+y**2)**2
+    return stress_xx
+def edge_stress_yy(x,y,shear_modulus,burgers_vector,poissons_ratio):
+    D=shear_modulus*burgers_vector/(2*np.pi*(1-poissons_ratio))
+    stress_yy=D*y*(x**2-y**2)/(x**2+y**2)**2
+    return stress_yy
+def edge_stress_zz(x,y,shear_modulus,burgers_vector,poissons_ratio):
+    stress_zz=poissons_ratio*edge_stress_xx(x,y,shear_modulus,burgers_vector,poissons_ratio)*edge_stress_yy(x,y,shear_modulus,burgers_vector,poissons_ratio)
+    return stress_zz
 shear_modulus=90e9 # Pa
 burgers_vector=0.25e-9 #m
 poissons_ratio=0.3
@@ -31,27 +42,30 @@ x0=10e-9
 y0=10e-9
 #get stress values for center points
 stress=edge_stress_xy(xc_2d,yc_2d,shear_modulus,burgers_vector,poissons_ratio)
+plt.pcolormesh(xv_2d,yv_2d,stress)
 im=plt.imshow(stress,origin='lower',extent=(x_start,x_end,y_start,y_end),aspect='equal',vmin=-0.1e9,cmap='RdBu')
 plt.colorbar();
+#For lines display
 cs=plt.contour(xc_2d,yc_2d,stress,levels=np.linspace(-0.1e9,0.1e9,11),cmap='gray_r')
-plt.clabel(cs, fmt='%1.1e');
+#For values display
+plt.clabel(cs, fmt='%1.1e'); 
 sigma_xx=edge_stress_xy(xc_2d,yc_2d,shear_modulus,burgers_vector,poissons_ratio)
-fig,ax=plt.subplots(nrows=2,ncols=3,figsize=(15,10))
+##fig,ax=plt.subplots(nrows=2,ncols=3,figsize=(15,10))
 #Dictonories
-im_kwargs={'origin':'lower','extent':(x_start,x_end,y_start,y_end),'aspect':'equal','vmin':-0.1e9,'cmap':'RdBu'}
+##im_kwargs={'origin':'lower','extent':(x_start,x_end,y_start,y_end),'aspect':'equal','vmin':-0.1e9,'cmap':'RdBu'}
 #either of two
 #ax[0][0].imshow(sigma_xx,origin='lower',extent=(x_start,x_end,y_start,y_end),aspect='equal',vmin=-0.1e9,cmap='RdBu')
 #ax[0][0].imshow(sigma_xx, **im_kwargs)
 #ax[0][1].imshow(sigma_yy, **im_kwargs)
 #plt.colorbar()
-ax[0][0].set(xlabel='x[m]');
-ax[0][0].set(xlabel='x[m]',ylabel='y [m]');
+##ax[0][0].set(xlabel='x[m]');
+##ax[0][0].set(xlabel='x[m]',ylabel='y [m]');
 #plt.show()
 #fig, ax=plt.subplots(figsize=(10,10))
-fig, ax=plt.subplots(ncols=3, figsize=(20,7))
-sigma_xy_0=edge_stress_xy(xc_2d,yc_2d,shear_modulus,burgers_vector,poissons_ratio)
-sigma_xy_1=edge_stress_xy(xc_2d-x0,yc_2d-y0,shear_modulus,burgers_vector,poissons_ratio)
-ax[0].imshow(sigma_xy_0, **im_kwargs);
-ax[1].imshow(sigma_xy_1, **im_kwargs);
-ax[2].imshow(sigma_xy_0+sigma_xy_1, **im_kwargs);
+##fig, ax=plt.subplots(ncols=3, figsize=(20,7))
+##sigma_xy_0=edge_stress_xy(xc_2d,yc_2d,shear_modulus,burgers_vector,poissons_ratio)
+##sigma_xy_1=edge_stress_xy(xc_2d-x0,yc_2d-y0,shear_modulus,burgers_vector,poissons_ratio)
+##ax[0].imshow(sigma_xy_0, **im_kwargs);
+##ax[1].imshow(sigma_xy_1, **im_kwargs);
+##ax[2].imshow(sigma_xy_0+sigma_xy_1, **im_kwargs);
 plt.show()
